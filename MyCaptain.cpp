d13726acf51cc529447b294dab5288439a6ab68a
Hokage099 <99265017+Hokage099@users.noreply.github.com>
2022-02-10 21:04:08 +0530

    MyCaptain.cpp
    Assignment

---
+++ MyCaptain.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    cout << "sizeof(char) : "<< sizeof(char) << endl;
+    cout << "sizeof(int) : "<< sizeof(int) << endl;
+    cout << "sizeof(float) : "<< sizeof(float) << endl;
+    cout << "sizeof(double) : "<< sizeof(double) << endl << endl;
+
+    float a=3.4, b=5.5;
+    cout << "Product of 3.4 and 5.5 is " <<a*b;
+
+    return 0;
+}
